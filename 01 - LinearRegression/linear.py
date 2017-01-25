5f70bd8447e965df89625a4064bfe8637ea2b122
kparkk2 <eliath2@naver.com>
2017-01-26 01:28:16 +0900

    Changes due to Python version diff(2.7->3.5)

--- 01 - LinearRegression/linear.py
+++ 01 - LinearRegression/linear.py
@@ -7,6 +7,19 @@ y_data = [1., 2., 3.]
 w = tf.Variable(tf.random_uniform([1], -1.0, 1.0))
 b = tf.Variable(tf.random_uniform([1], -1.0, 1.0))
 
+###############################
+# tf.random_uniform(shape, minval=0, maxval=None, dtype=tf.float32, seed=None, name=None)
+###############################
+# Args
+#
+#   shape: A 1-D integer Tensor or Python array. The shape of the output tensor.
+#   minval: A 0-D Tensor or Python value of type dtype. The lower bound on the range of random values to generate. Defaults to 0.
+#   maxval: A 0-D Tensor or Python value of type dtype. The upper bound on the range of random values to generate. Defaults to 1 if dtype is floating point.
+#   dtype: The type of the output: float32, float64, int32, or int64.
+#   seed: A Python integer. Used to create a random seed for the distribution. See set_random_seed for behavior.
+#   name: A name for the operation (optional).
+###############################
+
 # my hypothesis
 hypothesis = w * x_data + b
 
@@ -26,9 +39,9 @@ sess = tf.Session()
 sess.run(init)
 
 # fit the line
-for step in xrange(2001):
+for step in range(2001):
     sess.run(train)
     if step % 20 == 0:
-        print step, sess.run(cost), sess.run(w), sess.run(b)
+        print( step, sess.run(cost), sess.run(w), sess.run(b) )
 
 # learns best fit is w: [1] b: [0]
--- 01 - LinearRegression/linearPlaceHolder.py
+++ 01 - LinearRegression/linearPlaceHolder.py
@@ -11,6 +11,35 @@ b = tf.Variable(tf.random_uniform([1], -10000., 10000.))
 
 X = tf.placeholder(tf.float32)
 Y = tf.placeholder(tf.float32)
+###############################
+# tf.placeholder(dtype, shape=None, name=None)
+###############################
+# Inserts a placeholder for a tensor that will be always fed.
+#
+# Important: This tensor will produce an error if evaluated. Its value must be fed using the feed_dict optional argument to Session.run(), Tensor.eval(), or Operation.run().
+#
+# For example:
+#
+#   x = tf.placeholder(tf.float32, shape=(1024, 1024))
+#   y = tf.matmul(x, x)
+#
+#   with tf.Session() as sess:
+#   print(sess.run(y))  # ERROR: will fail because x was not fed.
+#
+#   rand_array = np.random.rand(1024, 1024)
+#   print(sess.run(y, feed_dict={x: rand_array}))  # Will succeed.
+#
+# Args:
+#
+#   dtype: The type of elements in the tensor to be fed.
+#   shape: The shape of the tensor to be fed (optional). If the shape is not specified, you can feed a tensor of any shape.
+#   name: A name for the operation (optional).
+#
+# Returns:
+#
+#   A Tensor that may be used as a handle for feeding a value, but not evaluated directly.
+###############################
+
 
 # my hypothesis
 hypothesis = W * X + b
@@ -31,10 +60,10 @@ sess = tf.Session()
 sess.run(init)
 
 # fit the line
-for step in xrange(2001):
+for step in range(2001):
     sess.run(train, feed_dict={X: x_data, Y: y_data})
     if step % 20 == 0:
-        print step, sess.run(cost, feed_dict={X: x_data, Y: y_data}), sess.run(W), sess.run(b)
+        print( step, sess.run(cost, feed_dict={X: x_data, Y: y_data}), sess.run(W), sess.run(b) )
 
-print sess.run(hypothesis, feed_dict={X: 5})
-print sess.run(hypothesis, feed_dict={X: 2.5})
+print( sess.run(hypothesis, feed_dict={X: 5}) )
+print( sess.run(hypothesis, feed_dict={X: 2.5}) )
